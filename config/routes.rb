254c8b373735312821e146d0c4840a65cd50da26
Dongjae Heo <lindsay.heo@gmail.com>
2023-06-01 17:07:52 +0100

    scroll and reset text

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   end
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'uset/:id', to: 'users#show', as: 'user'
+  get 'user/:id', to: 'users#show', as: 'user'
 end
